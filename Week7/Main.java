5a43f7b07a21bf14e9b2a504b2f73e0f6ab19859
DhafinRizqiF <95175164+DhafinRizqiF@users.noreply.github.com>
2022-04-09 11:35:10 +0700

    Add files via upload

---
+++ Week7/Main.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Week7;
+
+/**
+ *
+ * @author Dhafin
+ */
+public class Main {
+    // main method creates and runs the ATM
+   public static void main(String[] args) {
+      ATM theATM = new ATM();    
+      theATM.run();
+}
+}
